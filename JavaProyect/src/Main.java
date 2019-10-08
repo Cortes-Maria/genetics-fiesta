93c43f3d8042888c5ad31da67934c6403518f6ce
Esteban Jiménez <44732656+JesusJimenezValverde@users.noreply.github.com>
2019-10-07 21:54:53 -0600

    Update Main.java

--- JavaProyect/src/Main.java
+++ JavaProyect/src/Main.java
@@ -153,12 +153,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         //Pasos
-        ArrayList<Zone> zonas = new ArrayList<Zone>();
-        // 1. generar el array de zonas ArrayList<Zone> cuadrantes = new ArrayList<Zone>();
-
-        //2. BufferedImage image = ImageIO.read(new File("dog.jpg"));
-
-        //3. muestreo(cuadrantes, cantDeMuestras, image);
 
 
     }
